9c278523c0d8324c3c1de75783d7498eb66fcaef
jacopolegovini <legovini96@gmail.com>
2024-11-14 17:01:55 +0100

    exercise completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -33,7 +33,7 @@ export default {
                 <div class="card-body">
                     <h5 class="card-title">{{ project.title }}</h5>
                     <p class="card-text">{{ project.description }}</p>
-                    <a href="#" class="btn btn-primary">Go Back</a>
+                    <a href="#" class="btn btn-primary">Done</a>
                 </div>
             </div>
         </div>
